079a11670d121e43dd5f515d327788de31caa8fc
Doug (on pi) <dugweb@gmail.com>
2015-05-07 15:57:13 +0000

    some dumb changes trying to get php to exec python

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php ini_set('display_errors', 'On'); ?>
+<html>
+<head>
+<meta charset="UTF-8" />
+</head>
+
+
+<?php
+if (isset($_POST['LightON']))
+{
+var_dump($_POST['LightON']);
+//$exec = exec("ping -c 3 -s 64 -t 64 8.8.8.8", $judist, $output);
+$exec = exec("sudo python /var/www/raspberrypi/8-Relay-Module/relay_test.py", $judist, $output);
+echo $exec;
+echo '<br />------------<br />';
+print_r($output);
+}
+?>
+
+<form method="post">
+<button class="btn" name="LightON" value="true">Test Relay</button>
+</form> 
+
+
+</html>
